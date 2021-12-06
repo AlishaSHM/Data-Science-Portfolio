246aad27aa2947580afaeb8d677e87e55596376f
AlishaSHM <alishashill@yahoo.com>
2021-12-05 20:55:42 -0600

    PROJECT 4

---
+++ SMU_PROJECT4_WAGEGAP/Website/modelHelper.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import datetime
+import time
+import pickle
+import numpy as np
+
+class ModelHelper():
+    def __init__(self):
+        pass
+
+    def makePredictions(self,gender, age,seniority, jobTitle, dpt):
+        features_np = ['Gender', 'Age', 'Seniority', 'JobTitle_Data Scientist',
+            'JobTitle_Driver', 'JobTitle_Financial Analyst',
+            'JobTitle_Graphic Designer', 'JobTitle_IT', 'JobTitle_Manager',
+            'JobTitle_Marketing Associate', 'JobTitle_Sales Associate',
+            'JobTitle_Software Engineer', 'JobTitle_Warehouse Associate',
+            'Dept_Administration', 'Dept_Engineering', 'Dept_Management',
+            'Dept_Operations', 'Dept_Sales']
+            
+        user_input = np.zeros(len(features_np))
+        user_input2 = np.zeros(len(features_np))
+        
+        job_idx = features_np.index(f'JobTitle_{jobTitle}')
+        dept_idx = features_np.index(f'Dept_{dpt}')
+
+
+        user_input[0] = gender
+        user_input[1] = age
+        user_input[2] = seniority
+        user_input[job_idx] = 1
+        user_input[dept_idx] = 1
+
+        user_input2[0] = 1 if gender == 0 else 0
+        user_input2[1] = age
+        user_input2[2] = seniority
+        user_input2[job_idx] = 1
+        user_input2[dept_idx] = 1
+        
+        filename = 'finalized_model2.sav'
+        reg_load = pickle.load(open(filename, 'rb'))
+        
+        preds = reg_load.predict([user_input, user_input2])
+        return preds
---
+++ SMU_PROJECT4_WAGEGAP/Website/static/js/calculator.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    console.log("Page Loaded");
+
+    $("#filter").click(function() {
+        makePredictions();
+    });
+});
+
+function makePredictions() {
+    var gender = $("#gender").val();
+    var age = $("#age").val();
+    var seniority = $("#seniority").val();
+    var jobTitle = $("#jobTitle").val();
+    var dpt = $("#department").val();
+ 
+    // create the payload
+    var payload = {
+        "gender": gender,
+        "age": age,
+        "seniority": seniority,
+        "jobTitle": jobTitle,
+        "department": dpt,
+    }
+
+    // Perform a POST request to the query URL
+    $.ajax({
+        type: "POST",
+        url: "/makePredictions",
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({ "data": payload }),
+        success: function(returnedData) {
+            // print it
+            console.log(returnedData);
+
+       
+
+            publishPreds (returnedData);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("Status: " + textStatus);
+            alert("Error: " + errorThrown);
+        }
+    });
+
+    function publishPreds (returnedData) {
+
+        // empty the div
+        $("#pred1").empty();
+        $("#pred2").empty();
+        $("#pred2title").empty();
+
+        let pred1 = Math.round(returnedData.prediction[0]);
+        let pred2 = Math.round(returnedData.prediction[1]);
+
+        //if male
+        if (returnedData.gender == 0) {
+            
+            let malepred = `<h1 id=male><i class="tf-profile-male">$${pred1}</h1>`;
+            let femalepred = `<h1 id=female><i class="tf-profile-female">$${pred2}</h1>`;
+            let pred2title = "A Female Would Make"
+            $("#pred1").append(malepred);
+            $("#pred2").append(femalepred);
+            $("#pred2title").append(pred2title)
+        
+        // if female
+        } else {
+
+            let femalepred = `<h1 id=female><i class="tf-profile-female"> $${pred1}</h1>`;
+            let malepred = `<h1 id=male><i class="tf-profile-male"> $${pred2}</h1>`;
+            let pred2title = "A Male Would Make"
+            $("#pred1").append(femalepred);
+            $("#pred2").append(malepred);
+            $("#pred2title").append(pred2title)
+        }
+    }
+}
